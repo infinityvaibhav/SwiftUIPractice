27166c30c3217ec12584d9a228d1fcd8af047972
Vaibhav Upadhyay <cognizant.upadhyay@Mercedes-Benz.com>
2022-07-12 16:02:43 +0530

    inheritance in class

--- 100DaysOfSwiftUI.playground/Contents.swift
+++ 100DaysOfSwiftUI.playground/Contents.swift
@@ -1,11 +1,32 @@
 
-class Game {
-    var score = 0 {
-        didSet {
-            print("Score is now \(score)")
-        }
+class Employee {
+    let hours: Int
+    
+    init(hours: Int) {
+        self.hours = hours
+    }
+    
+    func printSummary() {
+        print("I work \(hours) hour a day")
     }
 }
 
-var newGame = Game()
-newGame.score += 10
+class Developer: Employee {
+    func work() {
+        print("I am writing code for \(hours) hours")
+    }
+}
+
+class Manager: Employee {
+    func work() {
+        print("I am going to meeting for \(hours) hours")
+    }
+}
+
+let robert = Developer(hours: 8)
+let joseph = Manager(hours: 10)
+robert.work()
+joseph.work()
+
+let novall = Developer(hours: 8)
+novall.printSummary()
