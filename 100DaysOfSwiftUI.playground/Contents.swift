2bcab40fa47421025d8718f1a82faa60b5860082
Vaibhav Upadhyay <cognizant.upadhyay@Mercedes-Benz.com>
2022-07-12 15:41:33 +0530

    how to create class learned today

--- 100DaysOfSwiftUI.playground/Contents.swift
+++ 100DaysOfSwiftUI.playground/Contents.swift
@@ -1,30 +1,11 @@
 
-struct School {
-    static var studentCout = 0
-    
-    static func add(student: String)  {
-        print("\(student) joined the school")
-        studentCout += 1
+class Game {
+    var score = 0 {
+        didSet {
+            print("Score is now \(score)")
+        }
     }
 }
 
-
-School.add(student: "Taylor Swift")
-print(School.studentCout)
-
-struct AppData {
-    static let version = "1.3 beta 2"
-    static let saveFileName = "setting.json"
-    static let homeURL = "https://hackingwithswift.com"
-}
-
-AppData.version
-
-struct Employee {
-    let username: String
-    let password: String
-    
-    static let example = Employee(username: "infinityvaibhav", password: "abc@123")
-}
-
-Employee.example
+var newGame = Game()
+newGame.score += 10
