a8bda95e1aa89d20d9201afabfe255592014d2f9
Vaibhav Upadhyay <cognizant.upadhyay@Mercedes-Benz.com>
2022-07-13 13:33:54 +0530

    learned protocol

--- 100DaysOfSwiftUI.playground/Contents.swift
+++ 100DaysOfSwiftUI.playground/Contents.swift
@@ -1,18 +1,41 @@
-class Vehicle {
-    let isElectric: Bool
+protocol Vehicle {
+    var name: String { get }
+    var currentPassenger: Int { get set }
+    func estimateTime(for distance: Int) -> Int
+    func travel(distance: Int)
+}
+
+struct Car: Vehicle {
+    let name = "Car"
+    var currentPassenger = 1
     
-    init(isElectric: Bool) {
-        self.isElectric = isElectric
+    func estimateTime(for distance: Int) -> Int {
+        distance / 50
+    }
+     
+    func travel(distance: Int) {
+        print("I am driving \(distance) Km")
+    }
+    
+    func openSunRoof() {
+        print("Its a nice day!")
     }
 }
 
-class Car: Vehicle {
-    var isConvertable: Bool
-    
-    init(isElectric: Bool, isConvertable: Bool) {
-        self.isConvertable = isConvertable
-        super.init(isElectric: isElectric)
+func getTravelEstimates(using vehicles: [Vehicle], distance: Int) {
+    for vehicle in vehicles {
+        let estimate = vehicle.estimateTime(for: distance)
+        print("\(vehicle.name) \(estimate) hours to travel \(distance)Km")
     }
 }
 
-let teslaX = Car(isElectric: true, isConvertable: false)
+func commute(distance: Int, using vehicle: Car) {
+    if vehicle.estimateTime(for: distance) > 100 {
+        print("That's too slow! I'll try a different vehicle")
+    } else {
+        vehicle.travel(distance: distance)
+    }
+}
+
+let car = Car()
+commute(distance: 100, using: car)
